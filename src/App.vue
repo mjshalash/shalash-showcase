e31b165ad6467eaa12146ce99c6ce62fc452fe9d
Malik Shalash <mjshalash@rci4u.com>
2022-12-23 18:02:58 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import "~bootstrap/css/bootstrap.css"
+
+import MainContent from './components/MainContent.vue'
+import SideNav from './components/SideNav.vue'
+</script>
+
+<template>
+    <SideNav />
+</template>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '../assets/css/nav.css'
+import '~bootstrap/css/bootstrap.css'
+</script>
+
+<template>
+  <div id="nav-background">
+    <ul>
+      <li>
+        Hello World
+      </li>
+    </ul>
+  </div>
+</template>
